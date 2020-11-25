fd5238933cdef4e4ebbee9ac3e3bc10d0bfb96d6
G. Lan <64323783+G-Lan@users.noreply.github.com>
2020-11-25 15:53:35 -0500

    Update LikeRecordDAO.java

--- src/main/java/db/LikeRecordDAO.java
+++ src/main/java/db/LikeRecordDAO.java
@@ -10,18 +10,30 @@ import java.util.List;
 import java.util.Optional;
 
 public class LikeRecordDAO extends AbstractDAO<LikeRecord>{
+    /**
+     * Call parent constructor.
+     */
     public LikeRecordDAO(SessionFactory factory) {
         super(factory);
     }
-
+    
+    /**
+     * Change the like record by id. 
+     */
     public Optional<LikeRecord> findById(Long id) {
         return Optional.ofNullable(get(id));
     }
 
+    /**
+     * Create one like record. 
+     */
     public LikeRecord create(LikeRecord record) {
         return persist(record);
     }
 
+    /**
+     * Read all like records by the user's email. 
+     */
     @SuppressWarnings("unchecked")
     public List<LikeRecord> findAll(String email) {
         return list((Query<LikeRecord>) namedQuery("models.findAllLikes").
