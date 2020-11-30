c2377bb48c8f728a8492f75e97ffe2d4096b3a93
wxuejing <xw2668@columbia.edu>
2020-11-30 18:40:38 -0500

    solved conflict

--- src/main/java/db/UserProfileDAO.java
+++ src/main/java/db/UserProfileDAO.java
@@ -1,2 +1,28 @@
-package db;public class UserProfileDAO {
+package db;
+
+import io.dropwizard.hibernate.AbstractDAO;
+import models.UserProfile;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class UserProfileDAO extends AbstractDAO<UserProfile> {
+
+    public UserProfileDAO(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    /**
+     * create a user profile entry in db
+     * @param profile
+     * @return an UserProfile object being added to db
+     */
+    public UserProfile create(UserProfile profile) {
+        return persist(profile);
+    }
+
+    public List<UserProfile> getAll() {
+        return list((Query<UserProfile>) namedQuery("getAllUsers"));
+    }
 }
